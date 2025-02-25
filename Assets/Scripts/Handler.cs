f97a9ec704870a3a78afc4203aa7462fdce53e40
jesusjurado2000 <jesus2014.mono@gmail.com>
2025-02-24 22:01:10 -0500

    asda

---
+++ Assets/Scripts/Handler.cs
@@ -0,0 +1,138 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using TMPro;
+
+public class HttpHandler : MonoBehaviour
+{
+    [SerializeField] private string url = "";
+    [SerializeField] private TMP_InputField userIdInput;
+    [SerializeField] private Image[] cartasImages;
+    [SerializeField] private TMP_Text[] cartasText;
+    [SerializeField] private TMP_Text nameText;
+
+    private void Start()
+    {
+        if (gameObject.scene.rootCount != 0)
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public void SendRequest()
+    {
+        if (int.TryParse(userIdInput.text, out int userId))
+        {
+            StartCoroutine(GetUser(userId));
+        }
+        else
+        {
+            Debug.LogError("Por favor, ingresa un número válido.");
+        }
+    }
+
+    IEnumerator GetUser(int id)
+    {
+        UnityWebRequest www = UnityWebRequest.Get(url);
+        yield return www.SendWebRequest();
+
+        if (www.result != UnityWebRequest.Result.Success)
+        {
+            Debug.LogError("Error al conectar: " + www.error);
+        }
+        else
+        {
+            string jsonResult = "{\"users\":" + www.downloadHandler.text + "}";
+            UserData usersData = JsonUtility.FromJson<UserData>(jsonResult);
+
+            User personaje = usersData.users.Find(u => u.id == id);
+
+            if (personaje != null)
+            {
+                Debug.Log("Usuario encontrado: " + personaje.username);
+                nameText.text = personaje.username;
+                StartCoroutine(MostrarCartas(personaje.deck));
+            }
+            else
+            {
+                Debug.LogError("Usuario no encontrado.");
+            }
+        }
+    }
+
+    private IEnumerator MostrarCartas(List<Card> deck)
+    {
+        for (int i = 0; i < cartasImages.Length; i++)
+        {
+            if (i < deck.Count)
+            {
+                cartasText[i].text = deck[i].value;
+                yield return StartCoroutine(LoadImage(deck[i].image, cartasImages[i]));
+            }
+            else
+            {
+                cartasImages[i].gameObject.SetActive(false);
+                cartasText[i].text = "";
+            }
+        }
+    }
+
+    private IEnumerator LoadImage(string imageUrl, Image imageComponent)
+    {
+        UnityWebRequest request = UnityWebRequestTexture.GetTexture(imageUrl);
+        yield return request.SendWebRequest();
+
+        if (request.result != UnityWebRequest.Result.Success)
+        {
+            Debug.LogError("Error al cargar la imagen: " + request.error);
+        }
+        else
+        {
+            Texture2D texture = ((DownloadHandlerTexture)request.downloadHandler).texture;
+
+            Texture2D newTexture = new Texture2D(texture.width, texture.height, TextureFormat.RGBA32, false);
+            newTexture.SetPixels(texture.GetPixels());
+            newTexture.Apply();
+
+            Sprite newSprite = Sprite.Create(newTexture, new Rect(0, 0, newTexture.width, newTexture.height), Vector2.one * 0.5f);
+
+            if (imageComponent != null)
+            {
+                imageComponent.sprite = newSprite;
+                imageComponent.gameObject.SetActive(true);
+            }
+            else
+            {
+                Debug.LogError("El componente de imagen es nulo.");
+            }
+
+            request.Dispose();
+        }
+    }
+
+
+    [System.Serializable]
+    public class Card
+    {
+        public string value;
+        public string image;
+    }
+
+    [System.Serializable]
+    public class User
+    {
+        public int id;
+        public string username;
+        public bool state;
+        public List<Card> deck;
+    }
+
+    [System.Serializable]
+    public class UserData
+    {
+        public List<User> users;
+    }
+}
